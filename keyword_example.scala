c77ad30725e46d3052387cbe120a05639a5bbf69
Saurav Kumar <72607485+Saurav3218@users.noreply.github.com>
2021-04-30 19:01:39 +0530

    Update keyword_example.scala

--- keyword_example.scala
+++ keyword_example.scala
@@ -10,3 +10,29 @@ object Keyword_Example {
   }
 
 }
+
+
+-------------------
+// Here class keyword is used to create a new class
+// def keyword is used to create Function
+// var keyword is used to create a variable 
+class Student {
+
+  var name:String="Saurav";
+  var age:Int=25;
+  var branch:String="Electronics"
+
+  def show(): Unit ={
+    println("Hello! my name is "+ name + " and my age is" + age );
+    println("My branch is : "+ branch)
+
+  }
+}
+
+object Main{
+  def main(args: Array[String]): Unit = {
+    var s1=new Student();
+    s1.show();
+
+  }
+}
