0ee8e28c79e616d94858e78ccbf56d61986c4754
Saurav Kumar <72607485+Saurav3218@users.noreply.github.com>
2021-04-30 18:47:53 +0530

    keyword_example

---
+++ keyword_example.scala
@@ -0,0 +1,12 @@
+// Here object, def, and var are valid keywords
+
+object Keyword_Example {
+  def main(args: Array[String]): Unit = {
+
+    var p=10;
+    var q=50;
+    var sum=p+q;
+    println("The sum is : "+ sum)
+  }
+
+}
