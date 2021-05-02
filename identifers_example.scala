c78b0bbe60ff0f06290af726266acb52b15d57e2
Saurav Kumar <72607485+Saurav3218@users.noreply.github.com>
2021-05-02 15:50:57 +0530

    done.

---
+++ identifers_example.scala
@@ -0,0 +1,14 @@
+object Student2 {
+  def main(args: Array[String]): Unit = {
+   //valid identifiers
+    var name="Saurav"
+    var _age=25;
+    var Branch="Electronics"
+    var empId=121;
+    println("Name: "+ name +"\n" +"Branch "+Branch)
+
+
+
+  }
+
+}
