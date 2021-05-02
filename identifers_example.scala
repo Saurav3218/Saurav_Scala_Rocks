f4e8049c92a4c972583a94b8f75d4630e0869ea6
Saurav Kumar <72607485+Saurav3218@users.noreply.github.com>
2021-05-02 16:12:32 +0530

    Rules for variable

--- identifers_example.scala
+++ identifers_example.scala
@@ -12,3 +12,16 @@ object Student2 {
   }
 
 }
+
+
+'''
+Rules for naming variable in Scala
+
+Variable name should be in lower case.
+Variable name can contain letter, digit and two special characters(Underscore(_) and Dollar($) sign)
+Variable name must not contain the keyword or reserved word.
+Starting letter of the variable name should be an alphabet.
+White space is not allowed in variable name.
+
+'''
+
